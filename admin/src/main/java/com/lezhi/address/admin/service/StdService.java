ca4e0564b92d472b7d0f1bd7ca1fe4ff2251e00d
wangyinhua <27946298@qq.com>
2017-02-08 18:10:20 +0800

    匹配单条地址

---
+++ admin/src/main/java/com/lezhi/address/admin/service/StdService.java
@@ -0,0 +1,229 @@
+package com.lezhi.address.admin.service;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.stereotype.Service;
+
+import com.lezhi.address.admin.mapper.StdMapper;
+import com.lezhi.address.admin.pojo.ReturnParam;
+import com.lezhi.address.admin.pojo.StdModel;
+import com.lezhi.address.admin.util.AddressExtractor;
+
+/**
+ * Created by Cuill on 2016/12/12.
+ */
+@Service
+public class StdService {
+
+    @Autowired
+    private StdMapper mapper;
+
+    static SimpleDateFormat SD = new SimpleDateFormat("yyyy-MM-dd");
+    
+    public List<ReturnParam> analysis(String address) {
+        address = address.replaceAll(" ", "");
+        if (address.indexOf("下") + 1 == address.length()) {
+            address = address.replace("下", "");
+        }
+        List<ReturnParam> list = new ArrayList<>();
+        ReturnParam reParam = new ReturnParam();
+        if (null == address || "".equals(address)) {
+            reParam.setFlag("8");
+            list.add(reParam);
+            return list;
+        }
+
+        StdModel model = AddressExtractor.parseAll(new StdModel(address));
+        System.out.println("地址解析成功=" + model.toString());
+        if (model.getResidence() == null && model.getRoad() == null) {
+            reParam.setFlag("3");
+            list.add(reParam);// 解析失败
+            return list;
+        }
+        reParam.setFlag("1");
+
+        // 判断区县是否对应
+
+        /*if (model.getRoad() != null && model.getDistrict() != null) {
+            String districtName = null;
+            List<String> districtNameList = mapper.getDistrictByRoad(model.getRoad());
+            if (null != districtNameList && districtNameList.size() != 1) {
+                // 路对应多个区
+                boolean flag = false;
+                for (String name : districtNameList) {
+                    if (name.equals(model.getDistrict())) {
+                        flag = true;
+                        break;
+                    }
+                }
+                if (!flag) {
+                    reParam.setFlag("2");// 区县不对应
+                    list.add(reParam);
+                    return list;
+                }
+            } else {
+                districtName = districtNameList.get(0);
+                if (null == districtName) {
+                    reParam.setFlag("5");
+                    list.add(reParam);
+                    return list;
+                }
+                if (null != districtName && !districtName.equals(model.getDistrict())) {
+                    reParam.setFlag("2");// 区县不对应
+                    list.add(reParam);
+                    return list;
+                }
+            }
+        }*/
+
+        // 小区名称
+        if (null != model.getResidence() && model.getRoad() == null) {
+            list = mapper.getStdAddr(model);
+            if (null != list && list.size() > 1) {
+                for (ReturnParam param : list) {
+                    param.setFlag("1");
+                    if (null == model.getHouseNum()) {
+                        param.setHouseNo(null);
+                        if (null != param.getAddrCode()) {
+                            param.setAddrCode(param.getAddrCode().replace(
+                                    param.getAddrCode().substring(19, param.getAddrCode().length()), "0000"));
+                        }
+                    }
+                    if (null == model.getBuilding()) {
+                        param.setBuilding(null);
+                        if (null != param.getAddrCode()) {
+                            param.setAddrCode(param.getAddrCode().replace(
+                                    param.getAddrCode().substring(15, param.getAddrCode().length()),
+                                    "00000000"));
+                        }
+                    }
+                }
+
+                return list;
+            } else if (list.size() == 1) {
+                reParam = list.get(0);
+                reParam.setFlag("1");
+                if (null == model.getHouseNum()) {
+                    reParam.setHouseNo(null);
+                    if (null != reParam.getAddrCode()) {
+                    	reParam.setAddrCode(reParam.getAddrCode().replace(reParam.getAddrCode().substring(19, reParam.getAddrCode().length()),"0000"));
+                    }
+                }
+                if (null == model.getBuilding()) {
+                    reParam.setBuilding(null);
+                    if (null != reParam.getAddrCode()) {
+                    	reParam.setAddrCode(reParam.getAddrCode().replace(reParam.getAddrCode().substring(15, reParam.getAddrCode().length()),"00000000"));
+                    }
+                }
+                list = new ArrayList<>();
+                list.add(reParam);
+                return list;
+            } else {
+                // 去标准地址库中查询
+//                model.setRoad(model.getResidence());
+                ReturnParam stdAddr1 = this.getStdAddr1(model);
+                if (null == stdAddr1) {
+                    reParam = new ReturnParam();
+                    reParam.setFlag("4");
+                    list = new ArrayList<>();
+                    list.add(reParam);
+                    return list;
+                } else {
+                    list = new ArrayList<>();
+                    list.add(stdAddr1);
+                    return list;
+                }
+
+            }
+        }
+
+        // 路弄
+        if (null != model.getRoad() || model.getLane() != null) {
+            list.add(this.getStdAddr1(model));
+            return list;
+        }
+        return null;
+    }
+
+    // model去标准库中查询
+    private ReturnParam getStdAddr1(StdModel model) {
+        ReturnParam reParam = mapper.getStdAddr1(model);
+        if (null == reParam) {
+            if (null == model.getBuilding() && null == model.getHouseNum()) {
+            	reParam = new ReturnParam();
+                reParam.setFlag("4");
+            	return reParam;
+            }
+            if (null != model.getHouseNum() && null != model.getBuilding()) {
+            	model.setHouseNum(null);
+            	reParam = mapper.getStdAddr1(model);
+            	if (null != reParam) {
+            		reParam = new ReturnParam();
+                    reParam.setFlag("6");
+            	} else {
+            		model.setBuilding(null);
+            		reParam = mapper.getStdAddr1(model);
+            		if(null != reParam) {
+            			reParam = new ReturnParam();
+                        reParam.setFlag("7");
+            		} else {
+            			reParam = new ReturnParam();
+                        reParam.setFlag("4");
+            		}
+            	}
+            } else if(null == model.getHouseNum() && null != model.getBuilding()) {
+            	model.setBuilding(null);
+        		reParam = mapper.getStdAddr1(model);
+        		if(null != reParam) {
+        			reParam = new ReturnParam();
+                    reParam.setFlag("7");
+        		} else {
+        			reParam = new ReturnParam();
+                    reParam.setFlag("4");
+        		}
+            } else if(null != model.getHouseNum() && null == model.getBuilding()) {
+            	model.setHouseNum(null);
+        		reParam = mapper.getStdAddr1(model);
+        		if(null != reParam) {
+        			reParam = new ReturnParam();
+                    reParam.setFlag("6");
+        		} else {
+        			reParam = new ReturnParam();
+                    reParam.setFlag("4");
+        		}
+            }
+            return reParam;
+        }
+        reParam.setFlag("1");
+        if (null == model.getHouseNum()) {
+
+            reParam.setHouseNo(null);
+            if (null != reParam.getAddrCode()) {
+                reParam.setAddrCode(reParam.getAddrCode().replace(
+                        reParam.getAddrCode().substring(19, reParam.getAddrCode().length()), "0000"));
+            }
+        }
+
+        if (null == model.getBuilding()) {
+            reParam.setBuilding(null);
+            if (null != reParam.getAddrCode()) {
+                reParam.setAddrCode(reParam.getAddrCode().replace(
+                        reParam.getAddrCode().substring(15, reParam.getAddrCode().length()), "00000000"));
+            }
+        }
+        return reParam;
+    }
+
+    public static void main(String[] args) {
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
+                "classpath:/conf/applicationContext*.xml");
+        context.start();
+        StdService bean = context.getBean(StdService.class);
+        bean.analysis("武东路198");
+    }
+
+}
--- admin/src/main/java/com/lezhi/address/admin/service/impl/TaskServiceImpl.java
+++ admin/src/main/java/com/lezhi/address/admin/service/impl/TaskServiceImpl.java
@@ -1,7 +1,20 @@
 package com.lezhi.address.admin.service.impl;
 
+import java.sql.ResultSet;
+import java.util.Date;
+import java.util.List;
+
+import com.lezhi.address.admin.mapper.StdMapper;
+import com.lezhi.address.admin.mapper.TaskMapper;
 import com.lezhi.address.admin.pojo.Address;
+import com.lezhi.address.admin.pojo.OuterAddress;
+import com.lezhi.address.admin.pojo.ReturnParam;
+import com.lezhi.address.admin.pojo.Task;
+import com.lezhi.address.admin.service.StdService;
 import com.lezhi.address.admin.service.TaskService;
+import com.lezhi.address.admin.util.AddressExtractor;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 /**
@@ -9,9 +22,51 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class TaskServiceImpl implements TaskService {
+	@Autowired
+	private TaskMapper taskMapper;
+	@Autowired
+	private StdMapper stdMapper;
+	@Autowired
+	private StdService stdService;
     @Override
     public Address match(int analysisTaskId, int addressId, int operatorUserId) {
-        return null;
+    	//根据analysisTaskId获取
+    	Task task = taskMapper.getTaskInfo(analysisTaskId);
+    	String address = null;
+    	if(task!=null) {
+    		
+    		String sql = "SELECT "+task.getAddressColumn()+" FROM "+task.getTableName()+" WHERE id="+addressId;
+    		try {
+    			ResultSet rs = AddressExtractor.connJDBC(task.getServer(), task.getDbSchema(), task.getUsername(), task.getPassword(), sql);
+    			while (rs.next()) {
+    				address = rs.getString(task.getAddressColumn());
+    			}
+    			if(address!=null) {
+    				List<ReturnParam> rList = stdService.analysis(address);
+    				if(rList!=null&&rList.size()>0) {
+    					OuterAddress outerAddress = new OuterAddress();
+    					outerAddress.setSrcTable(task.getTableName());
+    					outerAddress.setSrcTableId(task.getId());
+    					outerAddress.setStdAddrId(Integer.parseInt(rList.get(0).getId()));
+    					stdMapper.insertOuterAddress(outerAddress);
+    					Address reAdd = new Address();
+    					reAdd.setMatchStatus(10);
+    					reAdd.setMatchTime(new Date());
+    					return reAdd;
+    				} else {
+    					return null;
+    				}
+    			} else {
+    				return null;
+    			}
+    		} catch (Exception e) {
+				e.printStackTrace();
+				return null;
+			}
+    	} else {
+    		return null;
+    	}
+    	
     }
 
     @Override

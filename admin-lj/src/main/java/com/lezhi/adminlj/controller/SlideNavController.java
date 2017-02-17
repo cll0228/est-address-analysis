b69dc5fb78a0848a2b1e093d4140251603e4e8fa
wangyinhua <27946298@qq.com>
2017-02-17 15:48:44 +0800

    初始化页面操作

--- admin-lj/src/main/java/com/lezhi/adminlj/controller/SlideNavController.java
+++ admin-lj/src/main/java/com/lezhi/adminlj/controller/SlideNavController.java
@@ -1,13 +1,9 @@
 package com.lezhi.adminlj.controller;
 
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
-import javax.servlet.FilterChain;
-import javax.servlet.ServletRequest;
-import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
@@ -19,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.lezhi.adminlj.pojo.DataList;
 import com.lezhi.adminlj.pojo.District;
 import com.lezhi.adminlj.service.SlideNavService;
 
@@ -49,14 +46,26 @@ public class SlideNavController {
 		return result;
     }
     
-    @RequestMapping(value = "test", method = RequestMethod.GET)
+    @RequestMapping(value = "list", method = RequestMethod.GET)
     @ResponseBody
-    public Map<String, Object> test(@RequestParam(value = "id")String id,@RequestParam(value = "div")String div) throws Exception{
+    public Map<String, Object> list(HttpSession session, HttpServletRequest request, HttpServletResponse response) throws Exception{
     	Map<String, Object> result = new HashMap<>();
-    	String sb = id+"+"+div;
-    	System.out.println(sb);
-//    	ArrayList<District> dList = slideNavService.districtList();
-    	result.put("sb", sb);
+    	ArrayList<DataList> dataList = new ArrayList<DataList>();
+    	String qupin[] = new String[]{"pudongxinqu","minhang","baoshan","xuhui","putuo","yangpu","changning","songjiang","jiading","huangpu","jingan","zhabei","hongkou","qingpu","fengxian","jinshan","chongming"};
+    	String qu[] = new String[]{"浦东新区","闵行区","宝山区","徐汇区","普陀区","杨浦区","长宁区","松江区","嘉定区","黄浦区","静安区","闸北区","虹口区","青浦区","奉贤区","金山区","崇明区"};
+    	for (int i = 0; i < qupin.length; i++) {
+    		DataList da = new DataList();
+        	da.setDataId(qupin[i]);
+        	da.setHouseholds((int)((Math.random()*9+1)*100000));
+        	da.setProportion(1+(int)(Math.random()*90));
+        	da.setLatitude(31.2080020904541);
+        	da.setLongitude(121.60652923583984);
+        	da.setShowName(qu[i]);
+        	da.setType("init");
+        	
+        	dataList.add(da);
+		}
+    	result.put("dataList", dataList);
 		return result;
     }
 }

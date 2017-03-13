89704c7f881edcc07271c679f849dc57809c4ed0
cuill <cuill@orientail-finance.com>
2017-03-13 14:40:53 +0800

    包

--- statistics/src/main/java/com/lezhi/statistics/mapper/TestMapper.java
+++ statistics/src/main/java/com/lezhi/statistics/mapper/TestMapper.java
@@ -1,4 +1,4 @@
-package com.lezhi.adminlj.mapper;
+package com.lezhi.statistics.mapper;
 
 /**
  * Created by Colin Yan on 2017/2/13.
--- statistics/src/main/java/com/lezhi/statistics/pojo/PageResult.java
+++ statistics/src/main/java/com/lezhi/statistics/pojo/PageResult.java
@@ -1,4 +1,4 @@
-package com.lezhi.adminlj.pojo;
+package com.lezhi.statistics.pojo;
 
 /**
  * Created by Cuill on 2017/3/13.

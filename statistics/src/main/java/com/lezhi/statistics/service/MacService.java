54882ca0829eafce52c6b7bda9ab45d1da6fbbc1
chendl <chendl@oriental-finance.com>
2017-03-24 15:10:37 +0800

    数据平台日志详情查询

--- statistics/src/main/java/com/lezhi/statistics/service/MacService.java
+++ statistics/src/main/java/com/lezhi/statistics/service/MacService.java
@@ -1,7 +1,12 @@
 package com.lezhi.statistics.service;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import com.lezhi.statistics.pojo.MacVisit;
+import com.lezhi.statistics.pojo.MacVisitHistoryInfo;
+import com.lezhi.statistics.pojo.MacVisitLogInfo;
+import com.lezhi.statistics.util.ListPageUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -21,7 +26,6 @@ public class MacService {
 	 * @param type
 	 * @param id
 	 * @param start
-	 * @param end
 	 * @return
 	 */
 	public List<MacInfoObj> getMacInfoList(Integer type, Integer id,
@@ -62,5 +66,58 @@ public class MacService {
 	public MacInfoObj getMacInfo(String mac) {
 		MacInfoObj macInfo = macMapper.getMacInfo(mac);
 		return macInfo;
+	}
+
+	/**
+	 * 按时间段、频道、mac查日志详情列表
+	 * @param mac,channelNo..
+	 * @return
+	 */
+	public MacVisit getMacVisitLog(String mac, String channelNo, Long startTime, Long span, Integer districtId,
+									 Integer blockId, Integer residenceId,Integer pageNo, Integer pageSize) {
+		if (null != residenceId) {
+			districtId = null;
+			blockId = null;
+		} else if (null == residenceId && null != blockId) {
+			districtId = null;
+		}
+		Long start = 0L;
+		Long end = 0L;
+		if(startTime!=null&&!"".equals(startTime)) {
+			start = startTime - span;
+			end = startTime;
+		} else {
+			start = System.currentTimeMillis()/1000 - span;
+			end = System.currentTimeMillis()/1000;
+		}
+		List<MacVisitLogInfo> macInfoList = macMapper.getMacVisitLog(mac,channelNo,start,end,districtId,blockId,residenceId);
+		if (null == macInfoList || macInfoList.size() == 0) {
+			return new MacVisit("success", new ArrayList<MacVisit>(), "");
+		}
+		// 分页
+		MacVisit obj = new MacVisit();
+		ListPageUtil<MacVisitLogInfo> listPageUtil = new ListPageUtil<>(macInfoList, pageNo, pageSize);
+		List<MacVisitLogInfo> pagedList = listPageUtil.getPagedList();
+		obj.setHistories(pagedList);
+		obj.setStatus("success");
+		obj.setErrMsg("");
+		// 分页信息
+		if (pageNo == 1) {// 是否第一页
+			obj.setFirstPage(true);
+		} else {
+			obj.setFirstPage(false);
+		}
+		// 是否最后一页
+		int totalPage = listPageUtil.getTotalPage();
+		obj.setTotalPageCount(totalPage);
+		if (pageNo == totalPage) {
+			obj.setLastPage(true);
+		} else {
+			obj.setLastPage(false);
+		}
+		obj.setPageNo(pageNo);
+		obj.setPageSize(pageSize);
+		obj.setRealPageSize(pagedList.size());
+		return obj;
 	}
 }

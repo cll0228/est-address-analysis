a07c1218b82b527ac75761617504645b91de4c75
chendl <chendl@oriental-finance.com>
2017-03-29 15:21:33 +0800

    数据平台分钟刻度公共方法

--- statistics/src/main/java/com/lezhi/statistics/util/PropertyUtil.java
+++ statistics/src/main/java/com/lezhi/statistics/util/PropertyUtil.java
@@ -8,24 +8,6 @@ import java.util.Date;
  * Created by wangyh on 2017/3/28.
  */
 public class PropertyUtil {
-    /*public static String getProperty(String type, String key) {
-        InputStream is = null;
-        Properties dbProps = new Properties();
-        try {
-            is = PropertyUtil.class.getClassLoader().getResourceAsStream(type + ".properties");
-            dbProps.load(is);
-            return dbProps.getProperty(key);
-        } catch (Exception e) {
-            return null;
-        } finally {
-            if (null != is) try {
-                is.close();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-    }*/
-    
     public static String getStartHour(Date date) {  
         Calendar calendar = Calendar.getInstance();  
         calendar.setTime(date);  
@@ -52,8 +34,26 @@ public class PropertyUtil {
         date = calendar.getTime();  
         return sm.format(date);  
     }
+
+    public static String getStartMinute(Date date) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(date);
+        calendar.add(Calendar.MINUTE, -1);
+        SimpleDateFormat sm = new SimpleDateFormat("yyyy-MM-dd HH:mm:00");
+        date = calendar.getTime();
+        return sm.format(date);
+    }
+
+    public static String getEndMinute(Date date) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(date);
+        calendar.add(Calendar.MINUTE, -1);
+        SimpleDateFormat sm = new SimpleDateFormat("yyyy-MM-dd HH:mm:59");
+        date = calendar.getTime();
+        return sm.format(date);
+    }
     public static void main(String[] args) {
     	
     	System.out.println(getBeforeDay(new Date()));
-	}
+    }
 }
